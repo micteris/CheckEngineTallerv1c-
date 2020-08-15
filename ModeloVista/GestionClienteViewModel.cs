c4e447192be815d50dc4049eb77105bef079fc0f
Fredin Funez <fredinfu@live.com>
2020-08-15 13:38:42 -0600

    View Model   +Inicio View Model   +Gestion Cliente View Model

---
+++ ModeloVista/GestionClienteViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckEngineTaller.ModeloVista
+{
+    public class GestionClienteViewModel
+    {
+        public bool EsModoEdicion { get; set; }
+
+        public string CI_Direccion { get; set; }
+        public string CI_Identificacion { get; set; }
+        public string CI_Nombre1 { get; set; }
+        public string CI_Nombre2 { get; set; }
+        public string CI_Apellido1 { get; set; }
+        public string CI_Apellido2 { get; set; }
+        public string CI_Telefono { get; set; }
+        
+    }
+}
---
+++ ModeloVista/InicioViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckEngineTaller.ModeloVista
+{
+    public class InicioViewModel
+    {
+        public string E_Usuario { get; set; }
+        public string E_Password { get; set; }
+        public string ConfirmarPassword { get; set; }
+        public string E_Identificacion { get; set; }
+        public string E_Token { get; set; }
+
+    }
+}
